ccbe9dd0f74ce8f3e7815582ff58e9533b3a9083
faykey <fikayomicheal4@gmail.com>
2023-08-22 12:09:40 +0000

    indent

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -9,13 +9,13 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
         """Will display 'Hello HBNB'"""
-            return "Hello HBNB!"
+        return "Hello HBNB!"
 
 
         @app.route('/hbnb', strict_slashes=False)
         def hbnb():
                 """Will display 'HBNB'"""
-                    return "HBNB"
+                return "HBNB"
 
 
                 if __name__ == '__main__':
