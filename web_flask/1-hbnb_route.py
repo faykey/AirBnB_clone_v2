54f35c3ec9a0d0f9ab733162d69a66d70ae3135d
faykey <77450186+faykey@users.noreply.github.com>
2023-08-23 15:27:13 +0100

    Update 1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -8,15 +8,15 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-        """Will display 'Hello HBNB'"""
-        return "Hello HBNB!"
+    """Will display 'Hello HBNB'"""
+    return "Hello HBNB!"
 
 
-        @app.route('/hbnb', strict_slashes=False)
-        def hbnb():
-                """Will display 'HBNB'"""
-                return "HBNB"
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Will display 'HBNB'"""
+    return "HBNB"
 
 
-                if __name__ == '__main__':
-                        app.run(host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
