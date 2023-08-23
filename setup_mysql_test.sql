532994c62276b4368728812e46f8ea4042fd12a9
faykey <77450186+faykey@users.noreply.github.com>
2023-08-23 15:24:16 +0100

    Update setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,9 +1,8 @@
 -- Set ALL privileges on the database `hbnb_dev_db` to the `hbnb_dev` user.
 -- Set SELECT privileges on the database `performance_schema` to the `hbnb_dev` user.
 
-CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-DROP USER IF EXISTS 'hbnb_test'@'localhost';
-CREATE USER 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
